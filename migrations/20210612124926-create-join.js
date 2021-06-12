1c15ebe7d207e14d3f796a68700b1739524dd300
Kimseonghee <kimsunghee625@gmail.com>
2021-06-12 22:29:07 +0900

    [server] fix/models add join table

---
+++ migrations/20210612124926-create-join.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('joins', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      roomid: {
+        type: Sequelize.INTEGER
+      },
+      userid: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('joins');
+  }
+};
